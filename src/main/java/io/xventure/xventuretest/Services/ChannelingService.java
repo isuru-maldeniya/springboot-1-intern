709077f4349496595e938f3cd6ba956f262cb022
DELL <isurumaldeiya@gmail.com>
2021-10-04 14:30:55 +0530

    service layer done for Channeling

--- src/main/java/io/xventure/xventuretest/Services/ChannelingService.java
+++ src/main/java/io/xventure/xventuretest/Services/ChannelingService.java
@@ -1,7 +1,67 @@
 package io.xventure.xventuretest.Services;
 
+import io.xventure.xventuretest.controlles.DTO.ChannelDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
 
 @Service
 public class ChannelingService {
+
+    private LinkedList<ChannelDTO> dtos=new LinkedList<>();
+
+//    adding logic
+    public void addChanneling(ChannelDTO dto){
+        dtos.add(dto);
+    }
+
+//    all return logic
+    public ChannelDTO getChannel(int id ){
+        return ((LinkedList<ChannelDTO>)dtos.stream().filter(dto -> dto.getChannelId() == id)).element();
+    }
+
+    public LinkedList<ChannelDTO> getAllChannels() {
+        return dtos;
+    }
+
+//    update logic
+    public void updateChannel(ChannelDTO dto){
+        ChannelDTO channelDTO1=null;
+        for (ChannelDTO channelDTO : dtos) {
+            if(channelDTO.getChannelId()==dto.getChannelId()){
+                channelDTO1=channelDTO;
+                dtos.remove(channelDTO);
+            }
+        }
+
+        if(channelDTO1==null){
+            return;
+        }
+
+        if(dto.getEndTime()!=null){
+            channelDTO1.setEndTime(dto.getEndTime());
+        }
+        if(dto.getStartTime()!=null){
+            channelDTO1.setStartTime(dto.getStartTime());
+        }
+        if(dto.getNumOfPatients()!=0){
+            channelDTO1.setNumOfPatients(dto.getNumOfPatients());
+        }
+        if(dto.getDate()!=null){
+            channelDTO1.setDate(dto.getDate());
+        }
+        dtos.add(channelDTO1);
+    }
+
+//    delete logic
+    public void deleteChannel(int id){
+        for (ChannelDTO channelDTO : dtos) {
+            if(channelDTO.getChannelId()==id){
+                dtos.remove(channelDTO);
+            }
+        }
+    }
+
+
 }
--- src/main/java/io/xventure/xventuretest/controlles/ChannelController.java
+++ src/main/java/io/xventure/xventuretest/controlles/ChannelController.java
@@ -1,8 +1,10 @@
 package io.xventure.xventuretest.controlles;
 
 import io.xventure.xventuretest.Services.ChannelingService;
+import io.xventure.xventuretest.controlles.DTO.ChannelDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +14,13 @@ public class ChannelController {
 
     @Autowired
     private ChannelingService channelingService;
-    @PostMapping(value = "/addchannel")
-    public void addChanneling(){
 
+//    initiating new channel session for a doctorId
+    @PostMapping(value = "/addchannel")
+    public void addChanneling(@RequestBody ChannelDTO dto){
+        channelingService.addChanneling(dto);
     }
+
+
 
 }
